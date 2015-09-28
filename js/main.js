f7e6cbef59bb597d0eee567b355ae7e4a5798a58
salmansaleemkma <salman.saleemkma@gmail.com>
2015-09-28 10:01:30 +0530

    improved searchBox

---
+++ js/main.js
@@ -0,0 +1,125 @@
+var ProductList = React.createClass({
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url:this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr,status, err){
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	render: function() {
+		return (
+			<div className="row">
+			<div className="productList">
+			<h1 className="teal-text">Products</h1>
+			<Product data={this.state.data}></Product>
+			</div>      
+			</div>
+		);
+	}
+});
+
+var Product = React.createClass({
+	doSearch:function(queryText){
+		console.log(queryText)		
+			var queryResult = [];
+		console.log('from product:',this.props.data)
+			
+			this.props.data.forEach(function(product){
+				if(product.item_name.toLowerCase().indexOf(queryText)!=-1) {
+					queryResult.push(product);
+				}
+				else if(product.item_description.toLowerCase().indexOf(queryText)!=-1){
+					queryResult.push(product);
+				}
+				
+			});
+		
+		this.setState({
+			query: queryText,
+			filteredData: queryResult
+		})
+	},
+	getInitialState:function(){
+		return {
+			query:'',
+			filteredData: this.props.data
+		}
+	},
+	render: function() {
+		var passingData = [];
+		if(this.state.filteredData&&this.state.query == '') {
+			passingData = this.props.data;
+		} else {
+			passingData = this.state.filteredData;
+		}
+		return (
+			<div className="products">
+			<SearchBox query={this.state.query} doSearch={this.doSearch}/>
+			<SearchedProduct data={passingData}/>
+				</div>
+		);
+	}
+});
+
+var SearchBox = React.createClass({
+	doSearch:function(){
+		var query = this.refs.searchInput.getDOMNode().value;
+		this.props.doSearch(query);
+	},
+	render:function(){
+		return (
+			<div className="row">
+			<div className="col s6">
+			<div className="input-field">
+			<input type="search" ref="searchInput" value={this.props.query} onChange={this.doSearch}/>
+			<label>search</label>
+			<i className="material-icons">search</i>
+			</div>
+			</div>
+			</div>
+
+		);
+		
+	}
+});
+
+var SearchedProduct = React.createClass({
+	render: function() {
+		
+		var productNodes = this.props.data.map(function (product) {
+			return (	
+				<div className="col s4">
+				<div className="card-panel hoverable">
+				<div className="product">
+				<span className="right teal-text">{product.item_units} units</span>
+				<h5 className="truncate">{product.item_name}</h5>
+				<p className="truncate">{product.item_description}</p>
+				<span className="right chip">{product.item_type}</span>
+				<b>Rs.</b>{product.item_price} per unit.<br/>
+				</div>
+				</div>
+				</div>
+				
+			);
+		});
+		return (
+			<div className="searching-product">
+			{productNodes}
+			</div>
+		);
+	}
+});	
+
+
+React.render(
+	<ProductList url="http://127.0.0.1:5000/products" />,
+	document.getElementById('inventory-dashboard'));
